0b8fa77b3654ccb9a916ee1516e8d5b2f7d04322
drakkus1756 <8533103+drakkus1756@users.noreply.github.com>
2026-09-24 11:33:53 -0400

    Updated some upgrade chances, should be right? Still unsure on 24->25
    Renamed some html classes

--- starforce/script.js
+++ starforce/script.js
@@ -42,8 +42,6 @@ function simulateUpgrades(reqLevel, autoStarforce, currentStars, targetStars, nu
     let totalMesoCost = 0;
     let totalAttempts = 0;
 
-    
-
     for (let j = 0; j < numTrials; j++) {
         let upgradedStars = currentStars;
         let failStreak = 0;
@@ -78,14 +76,13 @@ function simulateUpgrades(reqLevel, autoStarforce, currentStars, targetStars, nu
 
 function getUpgradeChance(currentStars, autoStarforce, nxOption) {
     // manual @ 15000 NX - will be currentStars - 1
-    const upgradeChances = [100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 45, 45, 40, 40, 40, 40, 35, 35, 35, 35, 35, 30];
+    const upgradeChances = [100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 45, 45, 40, 40, 40, 40, 35, 35, 35, 35, 30, 25]; // Unsure on last 24->25, guessing 25%
     let upgradeChance = upgradeChances[currentStars - 1];
     if (autoStarforce) {
         upgradeChance -= 5;
     }
     
     return upgradeChance + 10 * nxOption;
-
 }
 
 function getMesoCost(reqLevel, currentStars) {
